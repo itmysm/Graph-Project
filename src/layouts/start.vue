320611f628f2b4b0d3aa6afc8dc05b01cdb6fae1
Meysam Kiani <dev.mysm@gmail.com>
2022-08-18 19:09:58 +0430

    feat: create loading component

--- src/layouts/start.vue
+++ src/layouts/start.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="layout">
-    <slot v-if="true"/>
+    <slot v-if="!loading"/>
+    <TheLoading v-if="loading" />
     <TheUnsupported v-if="false"/>
   </div>
 </template>
+
+<script setup>
+const loading = ref(true)
+
+onMounted(() => {
+  loading.value = false
+})
+</script>
