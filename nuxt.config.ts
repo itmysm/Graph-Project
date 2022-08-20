1f20ccc957c4503086cf6a25f063c1db9fe0df06
Meysam Kiani <dev.mysm@gmail.com>
2022-08-20 16:17:53 +0430

    chore: add nuxt/content to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'src',
-
+  modules: ['@nuxt/content'],
   buildModules: [],
   build: {
     postcss: {
--- src/components/dashboard/RecentFiles.vue
+++ src/components/dashboard/RecentFiles.vue
@@ -2,13 +2,13 @@
   <div class="">
     <h3 class="text-2xl text-secondary">Recently Files</h3>
     <div class="flex">
-      <ul class="mt-6">
-        <li class="w-52 mb-4 group cursor-pointer" @click="openFile(i)">
+      <ul class="flex mt-6">
+        <li class="w-52 mb-4 group cursor-pointer mr-5" @click="openFile(i)" v-for="data in examples" :key="data">
           <div class="overflow-hidden rounded-md relative">
             <img class="group-hover:blur-[2px] transition-all duration-500" src="~/assets/media/banners/socials/s-bf3594e61940024a1b16bffcada0c4a0.png" alt="">
             <NuxtLink class="absolute top-[40%] left-[29%] rounded-full"></NuxtLink>
           </div>
-          <p class="text-secondary mt-2 group-hover:text-primary transition-all duration-500">Whatsapp.html</p>
+          <p class="text-secondary mt-2 group-hover:text-primary transition-all duration-500">{{data.title}}.{{data._extension}}</p>
           <p class="text-xs text-red group-hover:text-primary transition-all duration-500">add 30days ago</p>
         </li>
       </ul>
@@ -16,8 +16,6 @@
   </div>
 </template>
 
-<script>
-// getting data from local or...
-// const data = reactive([])
-// function openFile(index){}
+<script setup>
+const { data: examples } = await useAsyncData('examples', () => queryContent('/examples').find())
 </script>
