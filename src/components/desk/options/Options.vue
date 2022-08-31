efb172d95ff9c55fc403cd3688eb0e933ba49afd
Meysam Kiani <dev.mysm@gmail.com>
2022-08-31 16:31:57 +0430

    feat: complete options section in desk page

--- src/components/desk/options/Options.vue
+++ src/components/desk/options/Options.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="w-fit min-w-[500px] max-w-[550px] h-[700px] flex flex-col justify-between rounded-md shadow-md px-10 py-4">
+  <div
+    class="w-fit min-w-[500px] max-w-[550px] max-fit flex flex-col justify-between rounded-md shadow-md px-10 py-4 transition-all durtion-500">
 
     <div class="w-full flex justify-between mb-5">
       <div>
-        <h1 class="font-bold text-xl">Choose what you want!</h1>
-        <p class="text-info text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ullam veniam</p>
+        <h1 class="font-bold text-xl" v-show="pageOption.currentPage == 1">Select Options!</h1>
+        <p class="text-neutral text-sm" v-show="pageOption.currentPage == 1">Select the options you would like to be shown in the results</p>
+
+        <h1 class="font-bold text-xl" v-show="pageOption.currentPage == 2">Additional items</h1>
+        <p class="text-neutral text-sm" v-show="pageOption.currentPage == 2">Tip: Choose OPM based on your computer's capabilities.</p>
+
+        <h1 class="font-bold text-xl" v-show="pageOption.currentPage == 3">Great.</h1>
+        <p class="text-neutral text-sm" v-show="pageOption.currentPage == 3">Click on the start button to start the analysis</p>
       </div>
-      <span class="flex text-nutral text-sm"><p>1</p> / <p>2</p></span>
+      <span class="flex text-neutral text-sm">
+        <p class="select-none">{{pageOption.currentPage}}</p> / <p>{{pageOption.pages}}</p>
+      </span>
     </div>
 
-    <div class="form-control">
+    <div class="form-control" v-show="pageOption.currentPage == 1">
       <label class="flex justify-start label cursor-pointer">
         <input type="checkbox" checked="checked" class="checkbox checkbox-sm" />
         <span class="label-text text-[16px] ml-3">Claculate number of messages</span>
@@ -71,18 +80,27 @@
       </label>
     </div>
 
-    <div class="flex mt-5">
-      <input type="range" min="10" max="100" value="50" class="range range-xs range-primary" />
-      <p class="min-w-fit text-xs ml-4">100 OPM</p>
+    <div class="flex mt-5" v-show="pageOption.currentPage == 2">
+      <input type="range" min="900" max="10000" value="2022" class="range range-xs range-primary" @input="getRangeValue" />
+      <div class="tooltip min-w-fit text-xs ml-1 cursor-default" data-tip="OPM: Operation in 1 miniute">
+        <p class=""> <span class="text-center inline-block w-[47px]">{{ range }}</span>OPM</p>
+      </div>
     </div>
 
     <div class="w-full flex justify-end mt-8">
-      <button class="btn btn-md btn-primary text-sm text-white disabled">Prev</button>
-      <button class="btn btn-md btn-primary ml-3 text-sm text-white">Next</button>
+      <button class="btn btn-md btn-primary text-sm text-white px-6" :class="pageOption.currentPage - 1 < 1 ? 'btn-disabled' : ''" @click="pageOption.currentPage--">Prev</button>
+      <button class="btn btn-md btn-primary ml-3 text-sm text-white px-6" :class="pageOption.currentPage >= pageOption.pages ? 'btn-disabled' : ''" @click="pageOption.currentPage++" v-if="pageOption.currentPage < pageOption.pages">Next</button>
+      <button class="btn btn-md btn-primary ml-3 text-sm text-white px-6" v-if="pageOption.currentPage == pageOption.pages">Start</button>
     </div>
   </div>
 </template>
 
 <script setup>
-  const range = ref(40)
+const range = ref(2022)
+const pageOption = reactive({ currentPage: 1, pages: 3 })
+
+function getRangeValue () {
+  range.value = document.querySelector('.range').value
+}
+
 </script>
--- src/pages/desk/index.vue
+++ src/pages/desk/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="w-full flex justify-center items-center">
     <TheLoading class="opacity-75 bg-white" v-show="false"/>
-    <DeskOptions/>
+    <div class="flex flex-col items-center justify-center h-full relative" v-if="!isFileExists">
+      <img class="group-hover:blur-[2px] transition-all duration-500" src="~/assets/media/banners/symbols/empty-folder.png" alt="" width="100">
+      <h3 class="text-secondary text-center mt-2">{{$t('infoSidebarNoFile')}}</h3>
+    </div>
+    <DeskOptions v-else/>
   </div>
 </template>
 
