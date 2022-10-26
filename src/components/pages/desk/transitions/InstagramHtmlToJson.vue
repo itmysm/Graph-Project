61d026fe8da3576a512927e8612dfa2df572959d
itmysm <dev.mysm@gmail.com>
2022-10-26 12:02:36 +0330

    fix: save time / date with correct format

--- src/components/pages/desk/transitions/InstagramHtmlToJson.vue
+++ src/components/pages/desk/transitions/InstagramHtmlToJson.vue
@@ -22,6 +22,13 @@ const data = {
   messages: []
 }
 
+const regex = {
+  getMonth: /((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))/,
+  getDay: /\d?\d/,
+  getYear: /\b(19|20)\d\d\b/,
+  getHour: /([01]?[0-9]|2[0-3]):[0-5][0-9]/
+}
+
 onMounted(async () => {
   if (fileInfo.application === 'instagram') {
     temp = document.querySelector('.instagram--temp')
@@ -45,11 +52,28 @@ function exportDataFromDOM () {
   // eslint-disable-next-line array-callback-return
   messages.map((msg) => {
     if (msg.querySelector('._a6-o').innerText !== '') {
-      data.messages.push({ name: msg.querySelector('._2pim')?.innerText, date: date.getTime(msg.querySelector('._a6-o')?.innerText), message: msg.querySelector('._a6-p div :nth-child(2)')?.innerText }) // stable for english lang not another
+      data.messages.push({ name: msg.querySelector('._2pim')?.innerText, date: convertDate(msg.querySelector('._a6-o')?.innerText), time: msg.querySelector('._a6-o')?.innerText.match(regex.getHour)[0] , message: msg.querySelector('._a6-p div :nth-child(2)')?.innerText }) // stable for english lang not another
     }
   })
 
   cleanDOM()
+}
+
+
+function convertDate (date) {
+  let month = getMonthFromString(date.match(regex.getMonth)[0])
+  let day = date.match(regex.getDay)[0]
+  let year = date.match(regex.getYear)[0]
+  let hour = date.match(regex.getHour)[0]
+  return toTimestamp(`${month}/${day}/${year} ${hour}:30`)
+}
+
+function getMonthFromString(mon){
+   return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1
+}
+
+function toTimestamp(strDate){
+  return Date.parse(strDate) / 1000
 }
 
 function cleanDOM () {
