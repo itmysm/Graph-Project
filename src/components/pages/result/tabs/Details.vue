7f0f9e0966a0260ad2165b32274d49d8db922566
Meysam Kiani <dev.mysm@gmail.com>
2022-12-06 18:10:45 +0330

    refactor: change content of detail tab

--- src/components/pages/result/tabs/Details.vue
+++ src/components/pages/result/tabs/Details.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="w-full">
-    <PagesResultTemplatesFileInfo />
-    <h4 class="mt-10 mb-3">Chat List</h4>
-    <!-- <PagesResultTemplatesMessages :messages="props.messages" /> -->
+    <PagesResultTemplatesMessagesBox :messages="props.messages" />
+    <div id="test1" class="grid grid-cols-2 xl:grid-cols-3 gap-10 w-full mt-6 mb-6">
+      <div class="w-full border border-accent rounded-md px-10">
+        <h1 class="mt-6 text-xl">{{$t('resultPageDetailsMessagesBoxAllMessages')}}</h1>
+        <PagesResultChartsBar :data="messages"/>
+      </div>
 
-    <h4 class="mt-10 mb-3">Chat List</h4>
-    <div class="grid grid-cols-2 gap-4">
-      <div class="...">01</div>
-      <!-- <PagesResultChartsBar :messages="props.messages" /> -->
+      <div class="w-full border border-accent rounded-md px-10">
+        <h1 class="mt-6 text-xl">{{$t('resultPageDetailsMessagesBoxAllMessages')}}</h1>
+        <PagesResultChartsBar :data="messages"/>
+      </div>
     </div>
   </div>
 </template>
