6ec58d2f84d2933f71c3695464ae205783da3b14
Meysam Kiani <dev.mysm@gmail.com>
2022-12-06 18:28:28 +0330

    feat: add loading to chart

--- src/components/pages/result/charts/Bar.vue
+++ src/components/pages/result/charts/Bar.vue
@@ -1,5 +1,8 @@
 <template>
 <v-chart class="w-full h-[400px]" :option="option" v-if="active" />
+<div class="flex justify-center items-center h-40" v-if="!active">
+  <AnimationsLoading class="w-full bg-primary" />
+</div>
 </template>
 
 <script setup>
