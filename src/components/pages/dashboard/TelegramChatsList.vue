01f6af878abe2633d55a0fd59b072e4986505491
Meysam Kiani <dev.mysm@gmail.com>
2022-12-06 18:09:29 +0330

    refactor: change animation to slideIn/slideOut

--- src/components/pages/dashboard/TelegramChatsList.vue
+++ src/components/pages/dashboard/TelegramChatsList.vue
@@ -92,57 +92,24 @@ function closePopUp (selectedItemIndex) {
 }
 
 @keyframes bounceIn {
-  from,
-  20%,
-  40%,
-  60%,
-  80%,
-  to {
-    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
-
-  0% {
-    opacity: 0;
-    transform: scale3d(0.3, 0.3, 0.3);
-  }
-
-  20% {
-    transform: scale3d(1.1, 1.1, 1.1);
-  }
-
-  40% {
-    transform: scale3d(0.9, 0.9, 0.9);
-  }
-
-  60% {
-    opacity: 1;
-    transform: scale3d(1.03, 1.03, 1.03);
-  }
-
-  80% {
-    transform: scale3d(0.97, 0.97, 0.97);
+  from {
+    transform: translate3d(0, 100%, 0);
+    visibility: visible;
   }
 
   to {
-    opacity: 1;
-    transform: scale3d(1, 1, 1);
+    transform: translate3d(0, 0, 0);
   }
 }
 
 @keyframes bounceOut {
-  20% {
-    transform: scale3d(0.9, 0.9, 0.9);
-  }
-
-  50%,
-  55% {
-    opacity: 1;
-    transform: scale3d(1.1, 1.1, 1.1);
+  from {
+    transform: translate3d(0, 0, 0);
   }
 
   to {
-    opacity: 0;
-    transform: scale3d(0.3, 0.3, 0.3);
+    visibility: hidden;
+    transform: translate3d(0, 100%, 0);
   }
 }
 </styles>
