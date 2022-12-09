c996bc75a424646a0462d5083bf514c58d1ec80c
itmysm <itmysm@gmail.com>
2022-12-09 22:08:47 +0330

    feat: Create Chart component that includes all the components of the charts and there is the ability to switch between them

--- src/components/pages/result/tabs/Details.vue
+++ src/components/pages/result/tabs/Details.vue
@@ -2,18 +2,23 @@
   <div class="w-full">
     <PagesResultTemplatesMessagesBox :messages="props.messages" />
     <div id="test1" class="grid grid-cols-1 xl:grid-cols-1 gap-10 w-full mt-6 mb-6">
-      <!-- <div class="w-full border border-accent rounded-md px-10">
+      <div class="w-full border border-accent rounded-md px-10">
         <h1 class="flex items-center mt-6 text-sm xl:text-lg">
-          <span class="flex items-center justify-center bg-primary/75 w-[30px] h-[30px] rounded-full ltr:mr-2 rtl:ml-2"><i class="material-symbols-rounded text-[20px] text-base-100">Info</i></span>
+          <span class="flex items-center justify-center bg-primary/75 w-[30px] h-[30px] rounded-full ltr:mr-2 rtl:ml-2"><i class="material-symbols-rounded text-[20px] text-white">Info</i></span>
           {{$t('resultPageDetailsMessagesBoxChartPerPersonMessage')}}
         </h1>
         <PagesResultChartsBar :data="messages"/>
-      </div> -->
+      </div>
 
       <div class="w-full border border-accent rounded-md px-10">
-        <h1 class="mt-6 text-xl">{{$t('resultPageDetailsMessagesBoxChartPerPersonMessage')}}</h1>
+        <h1 class="flex items-center mt-6 text-sm xl:text-lg">
+          <span class="flex items-center justify-center bg-primary/75 w-[30px] h-[30px] rounded-full ltr:mr-2 rtl:ml-2"><i class="material-symbols-rounded text-[20px] text-white">Info</i></span>
+          {{$t('resultPageDetailsMessagesBoxChartPerPersonMessage')}}
+        </h1>
         <PagesResultChartsSimpleLine :data="messagesInMonths"/>
       </div>
+
+      <PagesResultTemplatesChart :title="'resultPageDetailsMessagesBoxChartPerPersonMessage'" :data="messages" :activeCharts="chartTypes.type1" />
     </div>
   </div>
 </template>
@@ -27,5 +32,10 @@ const props = defineProps({
     required: true
   }
 })
+
+const chartTypes = {
+  type1: ['pie', 'bar'],
+  type2: ['pie', 'bar', 'line']
+}
 
 </script>
---
+++ src/components/pages/result/templates/Chart.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <ul class="flex">
+      <li class="flex items-start border border-accent border-b-0 rounded-t-md px-3 py-1 pt-2 rtl:ml-[1px] mr-[1px] cursor-pointer text-neutral"
+        :class="[activeIndex === index ? '!text-info' : '']"
+        v-for="(chart, index) in props.activeCharts" :key="index"
+        @click="activeIndex = index"
+        :="chartTypes[chart].status = true">
+
+        <span class=""><i class="material-symbols-rounded text-[20px]">{{chartTypes[chart].icon}}</i></span>
+        <p class="text-sm ltr:ml-1 rtl:mr-1">{{chart}}</p>
+
+      </li>
+    </ul>
+    <section class="w-full border border-accent rounded-b-md px-10">
+      <h1 class="flex items-center mt-6 text-sm xl:text-lg">
+          <span class="flex items-center justify-center bg-primary/75 w-[30px] h-[30px] rounded-full ltr:mr-2 rtl:ml-2"><i class="material-symbols-rounded text-[20px] text-white">Info</i></span>
+          {{$t(props.title)}}
+      </h1>
+
+        <div v-if="active">
+          <PagesResultChartsBar v-if="chartTypes.bar.status && activeIndex === returnIndexOfSpecifiedChart('bar')" :data="props.data" />
+          <PagesResultChartsPie v-if="chartTypes.pie.status && activeIndex === returnIndexOfSpecifiedChart('pie')" :data="props.data" />
+        </div>
+
+    </section>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    require: true
+  },
+  data: {
+    require: true
+  },
+  activeCharts: {
+    require: true
+  }
+})
+const theme = ref(document.querySelector('body').getAttribute('data-theme'))
+const chartTypes = { line: { icon: 'Stacked_Line_Chart', status: false }, bar: { icon: 'Bar_Chart', status: false }, pie: { icon: 'Pie_Chart', status: false } }
+const activeIndex = ref(1)
+const active = ref(true)
+
+function returnIndexOfSpecifiedChart (specifiedChart) {
+  let i = 0
+  props.activeCharts.forEach((chart, index) => {
+    if (specifiedChart === chart) i = index
+  })
+
+  return i
+}
+
+console.log(props.data)
+
+watch(() => activeIndex.value, () => {
+  active.value = false
+  setTimeout(() => {
+    active.value = true
+  }, 200)
+})
+
+</script>
