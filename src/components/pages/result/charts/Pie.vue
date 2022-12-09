ae6c066cb99f313eeecf7b6f3d1884615d47c0b2
itmysm <itmysm@gmail.com>
2022-12-09 22:06:51 +0330

    feat: add pie chart component

--- src/components/pages/result/charts/Pie.vue
+++ src/components/pages/result/charts/Pie.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-chart class="max-w-[500px]" :option="options" />
+  <v-chart class="w-full h-[400px]" :option="option" v-if="active" />
+  <div class="flex justify-center items-center h-40" v-if="!active">
+    <AnimationsLoading class="w-full bg-primary" />
+  </div>
 </template>
 
 <script setup>
@@ -8,40 +11,40 @@ const props = defineProps({
     required: true
   }
 })
-
-const options = {
+const active = ref(false)
+const option = {
   tooltip: {
     trigger: 'item'
   },
   series: [
     {
-      name: 'Messages',
+      name: 'Access From',
       type: 'pie',
-      radius: ['40%', '70%'],
-      avoidLabelOverlap: false,
-      itemStyle: {
-        borderRadius: 10,
-        borderColor: '#fff',
-        borderWidth: 2
-      },
-      label: {
-        show: false,
-        position: 'center'
-      },
+      radius: '70%',
+
+      data: Object.keys(props.data).map(val => {
+        if (val !== 'all') {
+          return { value: props.data[val].length, name: val }
+        } // This value(all) includes all the messages and does not create an interesting view for this graph, so we leave this value out of the loop.
+      }),
+
       emphasis: {
-        label: {
-          show: true,
-          fontSize: '40',
-          fontWeight: 'bold'
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
         }
-      },
-      labelLine: {
-        show: false
-      },
-      data: props.data.map(val => {
-        return { value: val.numOfMessages, name: val.name }
-      })
+      }
     }
   ]
 }
+
+onMounted(() => {
+  setTimeout(() => {
+    active.value = true
+  }, 500)
+  console.log(Object.keys(props.data).map(val => {
+    return { value: props.data[val], name: val }
+  }))
+})
 </script>
