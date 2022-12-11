48d3aba8b4dc1f56fd24499859f1cc7a68fc8114
Meysam Kiani <dev.mysm@gmail.com>
2022-12-11 13:03:48 +0330

    feat: add new section to details

--- src/components/pages/result/tabs/Details.vue
+++ src/components/pages/result/tabs/Details.vue
@@ -2,8 +2,10 @@
   <div class="w-full">
     <PagesResultTemplatesMessagesBox :messages="props.sortedMessages" />
     <div id="test1" class="grid grid-cols-1 xl:grid-cols-1 gap-10 w-full mt-6 mb-6">
-      <PagesResultTemplatesChart :title="'resultPageDetailsMessagesBoxChartPerPersonMessage'" :data="messages" :activeCharts="chartTypes.type1" />
-      <PagesResultTemplatesChart :title="'resultPageDetailsMessagesBoxChartPerMonth'" :data="messagesInMonths" :activeCharts="chartTypes.type2" />
+      <PagesResultTemplatesChart :title="'resultPageDetailsMessagesBoxChartPerPersonMessage'" :data="messages" :activeCharts="chartTypes.type2" />
+      <PagesResultTemplatesChart :title="'resultPageDetailsMessagesBoxChartPerMonth'" :data="messagesInMonths" :activeCharts="chartTypes.type3" />
+      <PagesResultTemplatesChart :title="'resultPageDetailsMessagesBoxChartTimeOfDay'" :data="timeMessages.timeOfDay" :activeCharts="chartTypes.type2" />
+      <PagesResultTemplatesChart :title="'resultPageDetailsMessagesBoxChartHoursOfDay'" :data="timeMessages.hoursOfDay" :activeCharts="chartTypes.type0" />
     </div>
   </div>
 </template>
@@ -18,12 +20,19 @@ const props = defineProps({
   },
   messagesInMonths: {
     required: true
+  },
+  timeMessages: {
+    required: true
   }
 })
 
+console.log(props.timeMessages)
+
 const chartTypes = {
-  type1: ['pie', 'bar', 'horizontalBar'],
-  type2: ['pie', 'horizontalBar', 'simpleLine']
+  type0: ['bar'],
+  type1: ['pie', 'bar'],
+  type2: ['pie', 'bar', 'horizontalBar'],
+  type3: ['pie', 'horizontalBar', 'simpleLine']
 }
 
 </script>
