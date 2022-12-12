353be45d26f31d08a122ac33d9e0e32d82e70541
Meysam Kiani <dev.mysm@gmail.com>
2022-12-12 11:03:15 +0330

    fix: fix AM OR PM calculation clock

--- src/scripts/calculations/time/hoursOfDay.js
+++ src/scripts/calculations/time/hoursOfDay.js
@@ -28,13 +28,14 @@ function convertTime12To24 (data, keyTime) {
   data.messages.forEach(msg => {
     if (msg[keyTime]) {
       const time = msg[keyTime]
-      let sHours = Number(time.match(/^(\d+)/)[1])
-      const AMPM = time.match(/\s(.*)$/)[1]
-      if (AMPM === 'PM' && sHours < 12) sHours = sHours + 12
-      if (AMPM === 'AM' && sHours === 12) sHours = sHours - 12
-      const mHours = sHours.toString()
+      let hour = +time.match(/^(\d+)/)[1] // => show hour ex: 12
 
-      hours['' + mHours] += 1
+      // eslint-disable-next-line prefer-const
+      let AM_OR_PM = time.match(/\s(.*)$/)[1] // => PM || AM
+
+      if (AM_OR_PM === 'PM' && hour < 12) hour = hour + 12
+      if (AM_OR_PM === 'AM' && hour === 12) hour = hour + 12
+      hours['' + hour] += 1
     }
   })
 }
