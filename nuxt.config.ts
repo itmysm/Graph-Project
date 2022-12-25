25db5afad5533191f6ac2ec65bc1fb6317d65e30
itmysm <itmysm@gmail.com>
2022-12-25 20:17:03 +0330

    Graph-project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/graph-project/'
+  },
   srcDir: 'src',
   modules: ['@nuxt/content',  '@pinia/nuxt',],
   build: {
