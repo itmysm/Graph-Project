5477155ac2cafa79d8e73be2603232ce02c276cd
itmysm <itmysm@gmail.com>
2023-01-29 02:44:48 +0330

    fix: tailwind now work

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/components/**/*.{js,vue,ts}",
-    "./src/pages/**/*.vue",
-    "./src/app.vue",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
