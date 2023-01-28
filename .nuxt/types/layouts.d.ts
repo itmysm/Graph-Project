2ae14e7255b0a2499e35ca30d1eb1ae2c040e8e2
itmysm <itmysm@gmail.com>
2023-01-29 03:11:39 +0330

    refactor: nothing remove OR add i just change the directory

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default" | "error" | "start"
-declare module "E:/Personal/Graph-Project/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
