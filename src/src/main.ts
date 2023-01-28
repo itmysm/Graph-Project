789d7e916d8af8b034a382cd810347ab0fd3b185
itmysm <itmysm@gmail.com>
2023-01-29 02:28:38 +0330

    chore: install tailwindcss & setup

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './assets/styles/tailwind.css' // tailwind styles
 
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
 
 const app = createApp(App)
 
---
+++ src/tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{js,vue,ts}",
+    "./src/pages/**/*.vue",
+    "./src/app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
