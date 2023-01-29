de1e008f11e438d838bf6d8841a616106ea2ab1f
itmysm <itmysm@gmail.com>
2023-01-29 21:28:55 +0330

    feat: setup themes for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,35 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/components/**/*.Vue",
   ],
   theme: {
+    colors: {
+      transparent: 'var(--color-transparent)',
+      white: 'var(--color-white)',
+      black: 'var(--color-black)',
+      background: 'var(--color-background)',
+
+      primary: 'var(--color-primary)',
+      'primary-content': 'var(--color-primary-content)',
+      'primary-active': 'var(--color-primary-active)',
+
+      secondary: 'var(--color-secondary)',
+      'secondary-content': 'var(--color-secondary-content)',
+      'secondary-active': 'var(--color-secondary-active)',
+
+      info: 'var(--color-info)',
+      'info-content': 'var(--color-info-content)',
+      'info-active': 'var(--color-info-active)',
+
+      neutral: 'var(--color-neutral)',
+      'neutral-content': 'var(--color-neutral-content)',
+      'neutral-active': 'var(--color-neutral-active)',
+
+      bermuda: 'var(--color-bermuda)',
+      error: 'var(--color-error)',
+      warning: 'var(--color-warning)',
+    },
     extend: {},
   },
   plugins: [],
