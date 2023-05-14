17760bef0605030f3f8523c3ba8d1e8b336e71da
Meysam Kiani <dev.mysm@gmail.com>
2023-05-14 10:26:14 +0300

    feat: now we have use i81n and use switcher for change lang

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import { appWithTranslation } from 'next-i18next'
+import { appWithI18Next, useSyncLanguage } from "ni18n";
+import { ni18nConfig } from "../../ni18n.config";
 
 import Application from '@/layouts/Application'
 import Default from '@/layouts/Default'
@@ -11,6 +12,10 @@ const layouts = {
 
 function App({ Component, pageProps }) {
   const Layout = layouts[Component.layout] || Default;
+  const settings = getSettings()
+  useSyncLanguage(settings.locale)
+  configDirection(settings.locale)
+
   return (
     <Layout>
       <Component {...pageProps} />
@@ -18,4 +23,20 @@ function App({ Component, pageProps }) {
   )
 }
 
-export default appWithTranslation(App)
+
+function getSettings() {
+  return {
+    theme: 'dark',
+    locale: 'fa',
+  }
+} // must be dynamic later
+
+function configDirection(locale) {
+  if (locale == 'fa') {
+    document.querySelector('body').setAttribute('dir', 'rtl')
+  } else {
+    document.querySelector('body').setAttribute('dir', 'ltr')
+  }
+}
+
+export default appWithI18Next(App, ni18nConfig)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,13 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 
-export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, [
-        'common',
-      ])),
-      // Will be passed to the page component as props
-    },
-  }
-}
 
 export default function Home() {
-  const { t } = useTranslation('common')
-
+  const { t } = useTranslation()
   return(
     <main
       className={`flex min-h-screen w-full font-inter`}>
         <p>{t('start')}</p>
+        <br />
     </main>
   )
 }
