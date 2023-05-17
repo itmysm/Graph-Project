cbe2a38d49e5a075fd6d2df8b9a0f6c31ea6f203
Meysam Kiani <dev.mysm@gmail.com>
2023-05-17 15:01:35 +0300

    chore: add nextui framework to project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import { appWithI18Next, useSyncLanguage } from "ni18n";
 import { ni18nConfig } from "../../ni18n.config";
 import { useEffect, useState } from 'react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+
 
 import Application from '@/layouts/Application'
 import Default from '@/layouts/Default'
@@ -24,9 +26,47 @@ function App({ Component, pageProps }) {
     document.querySelector('html').setAttribute('data-theme', theme);
   }, [dir, theme])
 
+
+  const nextUITheme = createTheme({
+    type: "dark", // it could be "light" or "dark"
+    theme: {
+      colors: {
+        transparent: 'var(--color-transparent)',
+        white: 'rgb(var(--color-white) / <alpha-value>)',
+        black: 'rgb(var(--color-black) / <alpha-value>)',
+        gray: 'rgb(var(--color-gray) / <alpha-value>)',
+        yellow: 'rgb(var(--color-yellow) / <alpha-value>)',
+        blue: 'rgb(var(--color-blue) / <alpha-value>)',
+        indigo: 'rgb(var(--color-indigo) / <alpha-value>)',
+        purple: 'rgb(var(--color-purple) / <alpha-value>)',
+        green: 'rgb(var(--color-green) / <alpha-value>)',
+        red: 'rgb(var(--color-red) / <alpha-value>)',
+        teal: 'rgb(var(--color-teal) / <alpha-value>)',
+
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        'primary-hover': 'rgb(var(--color-primary-hover) / <alpha-value>)',
+        'primary-active': 'rgb(var(--color-primary-active) / <alpha-value>)',
+
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        'secondary-hover': 'rgb(var(--color-secondary-hover) / <alpha-value>)',
+        'secondary-active': 'rgb(var(--color-secondary-active) / <alpha-value>)',
+
+        info: 'rgb(var(--color-info) / <alpha-value>)',
+        'info-hover': 'rgb(var(--color-info-hover) / <alpha-value>)',
+        'info-active': 'rgb(var(--color-info-active) / <alpha-value>)',
+
+        error: 'rgb(var(--color-error) / <alpha-value>)',
+        warning: 'rgb(var(--color-warning) / <alpha-value>)',
+        success: 'rgb(var(--color-success) / <alpha-value>)',
+      }
+    }
+  })
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <NextUIProvider theme={nextUITheme}>
+        <Component {...pageProps} />
+      </NextUIProvider>
     </Layout>
   )
 }
