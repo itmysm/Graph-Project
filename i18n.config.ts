221e5d583e9d4d2800ebeff07df4a147d65b6773
Meysam Kiani <dev.mysm@gmail.com>
2023-11-19 08:36:37 +0300

    refactor: remove de from languages

--- i18n.config.ts
+++ i18n.config.ts
@@ -2,10 +2,9 @@ export const i18n = {
   defaultLocale: "en",
   locales: [
     { label: "en", title: "English", dir: "ltr" },
-    { label: "de", title: "Dutch", dir: "ltr" },
     { label: "fa", title: "فارسی", dir: "rtl" },
   ],
 } as const;
 
 export type Locale = (typeof i18n)["locales"][number];
-export type LocaleLabel = "en" | "de" | "fa";
+export type LocaleLabel = "en" | "fa";
--- src/lib/locales.ts
+++ src/lib/locales.ts
@@ -3,7 +3,6 @@ import type { Locale, LocaleLabel } from "../../i18n.config";
 
 const locales = {
   en: () => import("../locales/en/translations.json").then((module) => module.default),
-  de: () => import("../locales/de/translations.json").then((module) => module.default),
   fa: () => import("../locales/fa/translations.json").then((module) => module.default),
 };
 
